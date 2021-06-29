66de74d9afc9d9f079511a58106e88230ae42125
akhilasanivada <86637218+akhilasanivada@users.noreply.github.com>
2021-06-29 15:52:38 +0530

    Add files via upload

---
+++ Reversenumbers.java
@@ -0,0 +1,18 @@
+  import java.util.Scanner;
+  public class Reversenumbers
+ {
+  private static Scanner sc;
+  public static void main(String[] args) 
+      {
+         int number, i;
+         sc = new Scanner(System.in);
+         System.out.print(" Please Enter the Maximum integer Value : ");
+         number = sc.nextInt();	
+         i = number;
+         while(i >= 1)
+      {
+         System.out.print(i +" "); 
+         i--;
+    }	
+  }
+}
